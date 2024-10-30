130c8cd695fda52504d92978e5c1e20b17beb586
Chris <chris79gp@outlook.com>
2024-10-30 18:48:20 +0000

    Feat: use react router to link to pages in `/pages` directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CareerCoaching from "./pages/CareerCoaching";
+import BusinessCoaching from "./pages/BusinessCoaching";
+import LifeCoaching from "./pages/LifeCoaching";
 import "./App.css";
 
 function App() {
 	return (
-		<>
-			<h1>Cluny House Consulting</h1>
-		</>
+		<div>
+			<nav>
+				<Link to="/">Home</Link>
+				<Link to="/business-coaching">Business Coaching</Link>
+				<Link to="/career-coaching">Career Coaching</Link>
+				<Link to="/life-coaching">Life Coaching</Link>
+			</nav>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/business-coaching" element={<BusinessCoaching />} />
+				<Route path="/career-coaching" element={<CareerCoaching />} />
+				<Route path="/life-coaching" element={<LifeCoaching />} />
+			</Routes>
+		</div>
 	);
 }
 
