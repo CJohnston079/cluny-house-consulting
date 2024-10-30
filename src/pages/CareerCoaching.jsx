0e900af61bbdc50dfbf49a78da366f4b8f2c8003
Chris <chris79gp@outlook.com>
2024-10-30 18:47:28 +0000

    Feat: add `/pages` directory with markup and copy for four pages

---
+++ src/pages/CareerCoaching.jsx
@@ -0,0 +1,117 @@
+const CareerCoaching = function () {
+	return (
+		<>
+			<h1>Career Coaching</h1>
+			<span>Achieve and Grow</span>
+			<h2>Unleash the Potential of You</h2>
+			<p>
+				Career coaching helps you plan the steps to achieve your life goals. Those that know what
+				they are trying to achieve can align their personal development and role choices to meet the
+				path laid out.
+			</p>
+			<p>Career coaching comes in three primary stages;</p>
+			<h3>Aspiring</h3>
+			<p>
+				For young people who are probably in their first role, and have reached the stage where they
+				are trying to decide what comes next. Typical questions Aspiring people ask themselves
+				include;
+			</p>
+			<ul>
+				<li>Do I stay where I am and seek promotion, or move on?</li>
+				<li>Have I made the right choice of career, or do I want to change?</li>
+				<li>Do I want to enter People Management, or develop as an Expertise Leader?</li>
+				<li>
+					What self-development should I be doing and how can I easily access quality materials?
+				</li>
+				<li>How do my personal goals fit with my career goals?</li>
+			</ul>
+			<p>
+				Getting the career foundations settled facilitates strong decision making, and also helps
+				employers see the plan you have made and the achievements along that path.
+			</p>
+			<h3>Strengthening</h3>
+			<p>
+				For people established in their career, looking to grow and get more senior roles, or maybe
+				thinking of a change and starting a new path. Typical questions Strengthening People ask
+				themselves is;
+			</p>
+			<ul>
+				<li>Am I on the right path, or is it time to change and go in another direction?</li>
+				<li>
+					I have been employed for some time, I know what I am doing, so is it time to strike out on
+					my own?
+				</li>
+				<li>What post-graduate qualifications do I aim to complete?</li>
+				<li>How do I get to the next stage in the career hierarchy?</li>
+			</ul>
+			<p>
+				For some this is a time to kick on and capitalise on experience gained, and for others, it
+				is a time to challenge themselves and create a new path. Wherever you sit, we can help you
+				gain direction, focus and a plan to succeed.
+			</p>
+			<h3>Strategic</h3>
+			<p>
+				For people in more senior roles, who have enjoyed success in their career often face
+				challenging decisions on entering the latter stages of a career, and are planning longer
+				term and into retirement. Typical questions Strategic people ask themselves include;
+			</p>
+			<ul>
+				<li>How do I plan for retirement?</li>
+				<li>What will I need to have the life I want for myself and my family?</li>
+				<li>How do I balance life and work?</li>
+				<li>What options do I have in my career?</li>
+				<li>Is it time to go and do something more for me?</li>
+			</ul>
+			<p>
+				The Strategic stage of your career is not about age. It reflects growing responsibilities,
+				higher demands on income, and the need to plan for the future to meet your personal, family
+				and business needs.
+			</p>
+			<p>Career coaching will help you to;</p>
+			<ul>
+				<li>Analyse where you are, and where you want to be.</li>
+				<li>Set goals for your personal life, family life and business life.</li>
+				<li>Confront limiting beliefs and other constraints.</li>
+				<li>Build out the steps to take to achieve your goals</li>
+			</ul>
+			<h2>How Do we do it?</h2>
+			<h3>Diagnose</h3>
+			<p>
+				This is an investigation of your situation. It involves you describing what is happening and
+				what is not happening, and you coach seeking clarification. At the end we understand what
+				your goals are and you can set your objectives.
+			</p>
+			<h3>Ideate</h3>
+			<p>
+				Now you understand what it is you are trying to do, the next part of the process is to
+				create ideas on potential actions. We explore what could be done, any development you need
+				and identify support structures.
+			</p>
+			<h3>Propose</h3>
+			<p>
+				You now move the ideas into actions with a plan that accounts for the actions, tasks,
+				development and support needed. You will also work on the change and communication plan,
+				making sure that you can bring all stakeholders along with you..
+			</p>
+			<h3>Execute</h3>
+			<p>
+				You take the proposal and implement it, evaluating using the measures and objectives we
+				formed earlier.
+			</p>
+			<h3>Review</h3>
+			<p>
+				The final stage is for you to review your progress and look again to the future, knowing
+				what has worked and what still requires work.
+			</p>
+			<h2>Want to find out more?</h2>
+			<p>
+				Before we get into the formal process, we start with an informal chat to establish if we are
+				a good fit to collaborate. In this meeting we will agree what happens next and the fee
+				structure and payment process.
+			</p>
+			<p>To get in touch, please use the contact form below, or email at xxx@xxx</p>
+		</>
+	);
+};
+
+export default CareerCoaching;
