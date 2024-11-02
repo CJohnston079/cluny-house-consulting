960a187c9677f9c4c76904cdff67f25097f73f8c
Chris <chris79gp@outlook.com>
2024-11-02 21:50:33 +0000

    Feat: improve accessibility of `Navigation` component

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -7,15 +7,15 @@ import logo from "./assets/cluny-house-consulting-logo.svg";
 const Navigation = function ({ routes }) {
 	return (
 		<header className="fb-col-wrapper nav-wrapper">
-			<nav className="navigation">
+			<nav className="navigation" role="navigation" aria-label="Main Navigation">
 				<Link id="home-link" className="nav-link" to="/">
-					<img className="nav-icon" src={logo} alt="" />
+					<img className="nav-icon" src={logo} alt="Cluny House Consulting logo" />
 					Cluny House Consulting
 				</Link>
 				<ul className="nav-links">
 					{routes.map(({ path, label, icon }) => (
 						<li key={path}>
-							<NavLink className="nav-link" to={path}>
+							<NavLink className="nav-link" to={path} aria-label={`Navigate to ${label}`}>
 								<img className="nav-icon" src={icon} alt="" />
 								{label}
 							</NavLink>
