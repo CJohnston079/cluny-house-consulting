969869fcc43454b3e6b7909ac28a96f913e51b24
Chris <chris79gp@outlook.com>
2024-11-15 22:19:52 +0000

    Style: apply BEM naming to `Hero` component

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,8 +4,8 @@ import "./Hero.css";
 const Hero = function ({ heading, description }) {
 	return (
 		<header className="hero fb-col-wrapper">
-			<h1 className="hero-heading">{heading}</h1>
-			<p className="hero-description">{description}</p>
+			<h1 className="hero__heading">{heading}</h1>
+			<p className="hero__description">{description}</p>
 		</header>
 	);
 };
