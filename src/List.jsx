4ca57e852bb08501057cdaf5dfd3b3856e0cddd6
Chris <chris79gp@outlook.com>
2024-11-17 12:32:06 +0000

    Feat: add `listHeading` prop to `List` which renders an `<h3>` element above the `<ul>`

--- src/List.jsx
+++ src/List.jsx
@@ -1,19 +1,23 @@
 import PropTypes from "prop-types";
 import "./List.css";
 
-const List = function ({ listItems }) {
+const List = function ({ listHeading, listItems }) {
 	return (
-		<ul className="list">
-			{listItems.map((listItem, i) => (
-				<li key={i} className="list__item">
-					{listItem}
-				</li>
-			))}
-		</ul>
+		<>
+			<h3 className="list__heading">{listHeading}</h3>
+			<ul className="list">
+				{listItems.map((listItem, i) => (
+					<li key={i} className="list__item">
+						{listItem}
+					</li>
+				))}
+			</ul>
+		</>
 	);
 };
 
 List.propTypes = {
+	listHeading: PropTypes.string,
 	listItems: PropTypes.arrayOf(PropTypes.string),
 };
 
