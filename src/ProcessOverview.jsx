640e57d55575bc4b313f61f5a66d7e1e7c1d5040
Chris <chris79gp@outlook.com>
2024-11-21 22:35:07 +0000

    Feat: update styling for `ProcessOverview` - Wrap `.process__step` elements in `.process__steps` container

--- src/ProcessOverview.jsx
+++ src/ProcessOverview.jsx
@@ -5,15 +5,17 @@ const ProcessOverview = function ({ heading, steps }) {
 	return (
 		<div className="process">
 			<h2 className="process__heading">{heading}</h2>
-			{steps.map((step, i) => (
-				<article key={i} className="process__step">
-					<header className="step__header">
-						<img className="step__icon" src={step.icon} alt="" />
-						<h3 className="step__heading">{step.heading}</h3>
-					</header>
-					<p className="step__body">{step.body}</p>
-				</article>
-			))}
+			<div className="process__steps">
+				{steps.map((step, i) => (
+					<article key={i} className="process__step">
+						<header className="step__header">
+							<img className="step__icon" src={step.icon} alt="" />
+							<h3 className="step__heading">{step.heading}</h3>
+						</header>
+						<p className="step__body">{step.body}</p>
+					</article>
+				))}
+			</div>
 		</div>
 	);
 };
