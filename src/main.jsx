2d95e887bafa1600a8f2b8396ffd6764f485af7d
Chris <chris79gp@outlook.com>
2025-01-16 20:56:05 +0000

    Refactor: split global styles into Sass partials in `globals/` and `layout/` directory

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
 import App from "./App";
-import "./index.scss";
+import "./scss/style.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
